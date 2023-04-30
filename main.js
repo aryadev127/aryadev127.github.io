71fc36d006c214471ade9a3d8f22857830d14860
aryadev127 <aryadewanggapuja@gmail.com>
2023-04-30 14:02:41 +0700

    portfolio github

---
+++ main.js
@@ -0,0 +1,19 @@
+// main of javascript
+
+import Nav from "./modules/js/nav-system.js";
+import candaan from "./modules/js/candaan.js";
+import setTime from "./modules/js/time.js";
+
+const nav = new Nav("btn-navon","nav-selct","nav-selection-on");
+nav.click;
+
+setInterval(setTime, 100);
+
+window.addEventListener("scroll", function() {
+  let nav = document.querySelector(".nav");
+  nav.classList.toggle("nav-onScroll", window.scrollY > 0)
+})
+
+
+candaan("joke")
+document.getElementById('joke').addEventListener('click', () => candaan('joke'))
